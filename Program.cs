a35e55f83a983da1ddf801fb0eb53b9a1ca3e57c
Theloopes <theloopesbr@gmail.com>
2023-10-23 19:59:20 -0300

    Exercicio completo

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("--- Multiplo ---");
+Console.WriteLine("");
+Console.WriteLine("Digite um numero");
+double numero = Convert.ToDouble(Console.ReadLine()!);
+Console.WriteLine("");
+Console.WriteLine("Avaliar se é multiplo de:");
+double multiplo = Convert.ToDouble(Console.ReadLine()!);
+double resultado = numero % multiplo;
+Console.WriteLine("");
+
+if(resultado == 0){
+    Console.WriteLine($"{numero} é multiplo de {multiplo}");
+}
+else{
+    Console.WriteLine($"{numero} não é multiplo de {multiplo}");
+}
